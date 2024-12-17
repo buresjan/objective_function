6f895a6396bbe3026a27cca50e83f1e69a641608
buresjan <bures.work@gmail.com>
2024-12-17 22:40:16 +0100

    add files

---
+++ shearrate.py
@@ -0,0 +1,81 @@
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+
+shear_rate_data = np.array(
+    [
+        7561.19687467,
+        8162.574,
+        8317.22471111,
+        8354.57411111,
+        8311.72182222,
+        8143.82944444,
+        7944.35662222,
+        6910.81295556,
+        6713.96935556,
+        6786.76435556,
+        6734.21746667,
+        6747.9641963,
+        6701.43491111,
+        6657.66053333,
+        6630.12906667,
+        6628.16809571,
+        6568.17995556,
+        6526.88277778,
+        6518.08355556,
+        6529.09684444,
+        6513.08897778,
+        6511.8096,
+        6494.05188889,
+        6504.91075556,
+        6472.63682222,
+    ]
+)
+
+offsets = np.array(
+    [
+        0.0,
+        0.1,
+        0.2,
+        0.3,
+        0.4,
+        0.5,
+        0.6,
+        0.7,
+        0.8,
+        0.9,
+        1.0,
+        1.1,
+        1.2,
+        1.3,
+        1.4,
+        1.5,
+        1.6,
+        1.7,
+        1.8,
+        1.9,
+        2.0,
+        2.1,
+        2.2,
+        2.3,
+        2.4,
+        2.5,
+        2.6,
+        2.7,
+    ]
+)
+
+
+def shear_rate(x, constraints=None):
+    if constraints is not None:
+        limit_low, limit_up = constraints
+        if np.any(x < limit_low) or np.any(x > limit_up):
+            return np.array([1e9])
+
+    tke_func = CubicSpline(offsets, shear_rate_data)
+
+    return tke_func(x)
+
+
+if __name__ == "__main__":
+    pass
---
+++ tke.py
@@ -0,0 +1,83 @@
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+tke_data = np.array(
+    [
+        385.2263103,
+        430.4428381,
+        452.5374252,
+        445.62689055,
+        429.1959315,
+        419.935845,
+        423.1248216,
+        487.31924113,
+        473.07342136,
+        411.54219,
+        376.59799,
+        360.375345,
+        354.511745,
+        285.84956,
+        271.51112475,
+        267.37261,
+        250.1182749,
+        250.546475,
+        227.6283432,
+        230.283652,
+        217.2345,
+        220.809905,
+        214.30512,
+        208.58241,
+        207.89333,
+        207.89333,
+        209.181435,
+        197.334675,
+    ]
+)
+
+offsets = np.array(
+    [
+        0.0,
+        0.1,
+        0.2,
+        0.3,
+        0.4,
+        0.5,
+        0.6,
+        0.7,
+        0.8,
+        0.9,
+        1.0,
+        1.1,
+        1.2,
+        1.3,
+        1.4,
+        1.5,
+        1.6,
+        1.7,
+        1.8,
+        1.9,
+        2.0,
+        2.1,
+        2.2,
+        2.3,
+        2.4,
+        2.5,
+        2.6,
+        2.7,
+    ]
+)
+
+
+def tke(x, constraints=None):
+    if constraints is not None:
+        limit_low, limit_up = constraints
+        if np.any(x < limit_low) or np.any(x > limit_up):
+            return np.array([1e9])
+
+    tke_func = CubicSpline(offsets, tke_data)
+
+    return tke_func(x)
+
+
+if __name__ == "__main__":
+    pass
